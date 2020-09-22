b39a95e1cfb44ff61cbc0fcf18ef7af95ef284fb
songminchao <839333033@qq.com>
2020-09-22 08:33:37 +0800

    lcw最新提交

--- src/main/java/controller/login/UserServlet.java
+++ src/main/java/controller/login/UserServlet.java
@@ -5,7 +5,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import factory.BeanFactory;
-import pojo.Admin;
 import pojo.User;
 import service.UserService;
 import javax.servlet.ServletException;
